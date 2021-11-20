1760d447b4c0f070678a47d69bc9d7445016a89d
StitchCodes <coderstitch@gmail.com>
2021-11-20 15:31:37 -0600

    Added initial variables for numbers and letters. Added test prompts and alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//  My code
+let pwdlength = prompt("how many characters? (1-128)");
+
+let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+let numbers = [1,2,3,4,5,6,7,8,9,0];
+
+if (pwdlength >1 && pwdlength <128) {
+  alert("You chose " + pwdlength + " characters.");
+}

91b15fab9dd030a97e440f04e288d24f599612e3
StitchCodes <coderstitch@gmail.com>
2021-11-21 12:00:03 -0600

    Added variables, functions and if statements for basic functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,56 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //  My code
-let pwdlength = prompt("how many characters? (1-128)");
 
-let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// let random = Math.floor(Math.random() * pwdlength.length);
+// console.log(random, months[random]);
 
-let numbers = [1,2,3,4,5,6,7,8,9,0];
+function generatePassword() {
+  // Variables
+  let pwdlength = prompt("How many characters? (8-128)");
 
-if (pwdlength >1 && pwdlength <128) {
-  alert("You chose " + pwdlength + " characters.");
+  let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+  let numbers = [1,2,3,4,5,6,7,8,9,0];
+
+  let specChars = [" ", "!", "#", "$", "%", "&", "(",")","*", "/", "_", "-", "="]
+
+  // Test IF for iteration
+  if (pwdlength >7 && pwdlength <128) {
+    alert("You chose " + pwdlength + " characters.");
+    
+    // Letters Option
+    let lettersOption = prompt("Include letters? yes/no");
+    if (lettersOption === "yes") {
+      alert("Your password will include letters.");
+    } else {
+      alert("Your password will NOT include letters.")
+    }
+    
+    // Numbers Option
+    let numbersOption = prompt("Include numbers? yes/no");
+    if (numbersOption === "yes") {
+      alert("Your password will include numbers.");
+    } else {
+      alert("Your password will NOT include numbers.")
+    }
+    
+    //  Special Characters Option
+    let specCharsOption = prompt("Include special characters? yes/no");
+    if (specCharsOption === "yes") {
+      alert("Your password will include special characters.");
+    } else {
+      alert("Your password will NOT include special characters.")
+    }
+
+    // If for password generator
+    if (lettersOption === "yes" && numbersOption === "yes" && specCharsOption === "yes") {
+      let random = Math.floor(Math.random() * pwdlength.length);
+      console.log(random);
+      return random;
+    }
+
+  } else {
+    alert("Please choose a number of characters between 8 and 128.");
+  }
 }

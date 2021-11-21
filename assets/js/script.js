5b713170cf4b9df190d16bf241febdd2a402f443
StitchCodes <coderstitch@gmail.com>
2021-11-21 16:50:23 -0600

    Added capital letters variable, added loop for generating pwd

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,54 +13,79 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//  My code
-
-// let random = Math.floor(Math.random() * pwdlength.length);
-// console.log(random, months[random]);
+//  ******
+// MY CODE
+//  ******
 
 function generatePassword() {
   // Variables
   let pwdlength = prompt("How many characters? (8-128)");
-
   let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+  let caps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   let numbers = [1,2,3,4,5,6,7,8,9,0];
+  let specChars = [",", "!", "#", "$", "%", "&", "(",")","*", "/", "_", "-", "="]
+  let options = [];
+  let gendPwd = "";
 
-  let specChars = [" ", "!", "#", "$", "%", "&", "(",")","*", "/", "_", "-", "="]
-
-  // Test IF for iteration
-  if (pwdlength >7 && pwdlength <128) {
-    alert("You chose " + pwdlength + " characters.");
+  // If with loop to get required values
+  if (pwdlength >7 && pwdlength <129) {
+    alert("You choose " + pwdlength + " characters.");
     
     // Letters Option
     let lettersOption = prompt("Include letters? yes/no");
-    if (lettersOption === "yes") {
+    if (lettersOption.toLowerCase() === "yes") {
       alert("Your password will include letters.");
+      options.push("letters");
     } else {
       alert("Your password will NOT include letters.")
+    }
+
+    // Caps Option
+    let capsOption = prompt("Include letters? yes/no");
+    if (capsOption.toLowerCase() === "yes") {
+      alert("Your password will include CAPITAL letters.");
+      options.push("caps");
+    } else {
+      alert("Your password will NOT include CAPITAL letters.")
     }
     
     // Numbers Option
     let numbersOption = prompt("Include numbers? yes/no");
-    if (numbersOption === "yes") {
+    if (numbersOption.toLowerCase() === "yes") {
       alert("Your password will include numbers.");
+      options.push("numbers");
     } else {
       alert("Your password will NOT include numbers.")
     }
     
     //  Special Characters Option
     let specCharsOption = prompt("Include special characters? yes/no");
-    if (specCharsOption === "yes") {
+    if (specCharsOption.toLowerCase() === "yes") {
       alert("Your password will include special characters.");
+      options.push("special")
     } else {
       alert("Your password will NOT include special characters.")
     }
 
-    // If for password generator
+    // If ALL yes THEN Generate Password
     if (lettersOption === "yes" && numbersOption === "yes" && specCharsOption === "yes") {
-      let random = Math.floor(Math.random() * pwdlength.length);
-      console.log(random);
-      return random;
+
+      // Iterate the password length for each of the choosen options
+      for (i=0; i < pwdlength; i++) {
+        // Get a Random Value from each string
+        let randomLetter = Math.floor(Math.random() * letters.length);
+        let randomNumber = Math.floor(Math.random() * numbers.length);
+        let randomSpChar = Math.floor(Math.random() * specChars.length);
+        
+        console.log("Letter choosen:" , randomLetter);
+        console.log("Letter choosen:" , randomNumber);
+        console.log("Letter choosen:" , randomSpChar);
+
+        // Sends text to global variable to print on textbox
+        return letters[random];
+
+      };
+      
     }
 
   } else {
